b84434a12c398d5bc594baf8e9160d9d26c47a6e
fthornton67 <fthornton67@gmail.com>
2018-08-07 16:51:08 -0700

    update attendee

--- src/app/modules/attendee/attendee.component.ts
+++ src/app/modules/attendee/attendee.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
+
 
 @Component({
   selector: 'app-attendee',
+ 
   templateUrl: './attendee.component.html',
   styleUrls: ['./attendee.component.css']
 })
 export class AttendeeComponent implements OnInit {
-
-  constructor() { }
+   @Input() name: string;
+  @Input() id: string;
+  constructor() {
+    this.name = 'Fredrick';
+    this.id = '0123456'
+   }
 
   ngOnInit() {
   }
